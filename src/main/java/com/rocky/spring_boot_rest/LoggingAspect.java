c1a51be7946b0825bb4cb30d3f22613ddfc20fb9
Nilesh <nileshjadhav0822@gmail.com>
2025-06-07 21:16:22 +0530

    spring boot rest added

---
+++ src/main/java/com/rocky/spring_boot_rest/LoggingAspect.java
@@ -0,0 +1,22 @@
+package com.rocky.spring_boot_rest;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    // return type class-name.method-name(args)
+    @Before("execution(* com.rocky.spring_boot_rest.service.JobService.getJob(..))")
+    public void logMethodCall(JoinPoint jp){
+        System.out.println("Method called " + jp.getSignature().getName());
+    }
+
+}
---
+++ src/main/java/com/rocky/spring_boot_rest/repository/JobRepo.java
@@ -0,0 +1,54 @@
+package com.rocky.spring_boot_rest.repository;
+
+import com.rocky.spring_boot_rest.model.JobPost;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class JobRepo {
+
+    List<JobPost> jobs = new ArrayList<>(Arrays.asList(
+            new JobPost(1, "Cloud Architect", "Design and implement cloud infrastructure solutions", 6, Arrays.asList("AWS", "Azure", "Docker")),
+            new JobPost(2, "Java Developer", "Must have good understanding in core java and advance java", 2, Arrays.asList("Spring Boot", "Hibernate", "JPA")),
+            new JobPost(3, "FrontEnd Developer ", "Must have good understanding in front end Technologis", 2, Arrays.asList("Html", "Css", "JavaScript"))
+
+    ));
+    public List<JobPost> getAllJobs(){
+        return jobs;
+    }
+
+    public void addJob(JobPost job){
+        jobs.add(job);
+        System.out.println(jobs);
+    }
+
+    public JobPost getJob(int postId) {
+        for(JobPost job :jobs){
+            if(job.getPostId()== postId){
+                return job;
+            }
+        }
+        return null;
+    }
+
+    public void updateJob(JobPost jobPost) {
+        for(JobPost jobPost1:jobs){
+            if(jobPost1.getPostId()==jobPost.getPostId()){
+                jobPost1.setPostProfile(jobPost.getPostProfile());
+                jobPost1.setPostDesc(jobPost.getPostDesc());
+                jobPost1.setReqExperience(jobPost.getReqExperience());
+                jobPost1.setPostTechStack(jobPost.getPostTechStack());
+            }
+        }
+    }
+
+    public void deleteJob(int postId) {
+        for(JobPost jobPost : jobs){
+            if(jobPost.getPostId()==postId)
+                jobs.remove(jobPost);
+        }
+    }
+}
